4b53d84fe2746e74d6f862182b62e4b74f750564
Karsten Jeppesen <kaje@ucn.dk>
2025-09-30 07:20:44 +0200

    Cars_Dapper_Console: initial commit

---
+++ Cars-Dapper-Console/Cars-Dapper-Console/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars_Dapper_Console
+{
+    public class Car
+    {
+        string licenseplate {  get; set; }
+        string make {  get; set; }
+        string model { get; set; }
+        string color { get; set; }
+
+    }
+}
---
+++ Cars-Dapper-Console/Cars-Dapper-Console/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Cars_Dapper_Console;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+
+
+string connectionString = "Data Source=localhost;Initial Catalog=Cars;User ID=sa;Password=@12tf56so;Trust Server Certificate=True";
+
+SqlConnection db = new SqlConnection(connectionString);
+db.Open();
+
+List<Car> Cars=db.Query<Car>("SELECT * FROM Cars").ToList();
+Console.WriteLine("");
